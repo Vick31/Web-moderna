f6ec339279dab742d09f266847bed8658b26b76c
Vick31 <manuelstivenperez@gmail.com>
2021-09-09 18:06:43 -0500

    actualizacion 23

--- js/codigo.js
+++ js/codigo.js
@@ -1,32 +1,33 @@
 $(document).ready(
 
     function () {
-
-        $padre = $('#inicio');
-        $hijo = $('#inicio .efecto');
-
-        $hijo.each(
-            function() {
-                var elemento = $(this); 
-
-                if(elemento.hasClass('e-right')) {
-                    elemento.css({right:'0px', filter: 'opacity(100%)'});                
-                }
-
-                if(elemento.hasClass('e-left')) {
-                    elemento.css({left:'0px', filter: 'opacity(100%)'});                
-                }
-
-                if(elemento.hasClass('e-top')) {
-                    elemento.css({top:'0px', filter: 'opacity(100%)'});                
-                }
-
-                if(elemento.hasClass('e-bottom')) {
-                    elemento.css({bottom:'0px', filter: 'opacity(100%)'});                
-                }
-            }
-        );
+        
+        deslizar( $('#inicio'), $('#inicio .efecto') );
 
     }
-
 );
+
+function deslizar(padre, hijo) {
+    hijo.each(
+        function() {    
+            var elemento = $(this); 
+
+            if(elemento.hasClass('e-right')) {
+                elemento.css({right:'0px', filter: 'opacity(100%)'});                
+            }
+
+            if(elemento.hasClass('e-left')) {
+                elemento.css({left:'0px', filter: 'opacity(100%)'});                
+            }
+
+            if(elemento.hasClass('e-top')) {
+                elemento.css({top:'0px', filter: 'opacity(100%)'});                
+            }
+
+            if(elemento.hasClass('e-bottom')) {
+                elemento.css({bottom:'0px', filter: 'opacity(100%)'});                
+            }
+        }
+    );
+}
+
